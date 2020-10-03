6667d9af73fc8307997af2ae4978309476ecf778
perkPerkins <37567887+perkPerkins@users.noreply.github.com>
2020-10-03 01:22:25 -0700

    Update network.py

--- onlineChess/network.py
+++ onlineChess/network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "73.157.134.80"
+        self.server = "WAN IP"
         self.port = 5000
         self.addr = (self.server, self.port)
         self.player_number = self.connect()
